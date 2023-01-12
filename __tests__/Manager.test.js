b6aec71b44a0b195187b33edeaa447115d1d484f
Emmalee Anderson <emilee.wimmer@gmail.com>
2023-01-12 13:27:58 -0700

    added manager tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,27 @@
+const Manager = require("../lib/Manager")
+const tName = 'test name';
+const tId = 'test id';
+const tEmail = 'test@email.com'
+const tOffice = '101'
+const tManager = new Engineer(tName, tId, tEmail, tOffice);
+
+test('validate constructor ', () => {
+    expect(tManager.name).toBe(tName);
+    expect(tManager.id).toBe(tId);
+    expect(tManager.email).toBe(tEmail);
+    expect(tManager.officeNumber).toBe(tOffice);
+});
+
+//test getters
+test('validate getRole', () => {expect(tManager.getRole()).toBe('Manager')});
+test('validate getGithub', () => {expect(tManager.getOfficeNumber()).toBe(tOffice)});
+test('validate getExtraStuff', () => {
+    expect(tManager.getExtraStuff()[0]).toBe('Office Number');
+    expect(tManager.getExtraStuff()[1]).toBe(tOffice);
+});
+
+//test inherited getters?
+test('validate inherited getName', () => {expect(tManager.getName()).toBe(tName)});
+test('validate inherited getEmail', () => {expect(tManager.getEmail()).toBe(tEmail)});
+test('validate inherited getId', () => {expect(tManager.getId()).toBe(tId)});
+//test('', () => {expect().toBe()})
